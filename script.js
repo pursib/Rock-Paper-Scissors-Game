d6a04edd01c1019bbfb345bc44979c78017045da
Preslav <pursib@abv.bg>
2021-08-21 10:11:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 const handOptions = {
-    "rock" : "/assets/Rock.png",
-    "paper" : "/assets/Paper.png",
-    "scissors" : "/assets/Scissors.png"
+    "rock" : "assets/Rock.png",
+    "paper" : "assets/Paper.png",
+    "scissors" : "assets/Scissors.png"
 }
 let SCORE = 0;
 
